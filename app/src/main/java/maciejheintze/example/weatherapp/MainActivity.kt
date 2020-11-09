bf2134aabf410ed6ce47f03888bec86b51cf14cf
Maciek <maciej.heintze96@gmail.com>
2020-11-09 12:30:20 +0100

    resolving input commit

--- app/src/main/java/maciejheintze/example/weatherapp/MainActivity.kt
+++ app/src/main/java/maciejheintze/example/weatherapp/MainActivity.kt
@@ -3,8 +3,10 @@ package maciejheintze.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 import maciejheintze.example.weatherapp.retrofit.MainViewModel
 import maciejheintze.example.weatherapp.retrofit.MainViewModelFactory
 import maciejheintze.example.weatherapp.retrofit.Repository
@@ -17,16 +19,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getInput()
+    }
 
+    private fun getInput(){
+        search_button_id.setOnClickListener {
+            if(!search_edit_text_id.text.isNullOrEmpty()){
+                val searchString = search_edit_text_id.text.toString()
+                fetchData(searchString)
+            }
+        }
+    }
+
+    private fun fetchData(value: String){
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getWeather("Warsaw", apiKey, "metric")
+        viewModel.getWeather(value, apiKey, "metric")
         viewModel.myResponse.observe(this, Observer { response ->
             if(response.isSuccessful){
                 Log.d("test_response", response.body()?.main?.temp.toString())
             }else{
-               Log.d("test_response", response.errorBody().toString())
+                Toast.makeText(this, "Wrong input! No data found", Toast.LENGTH_LONG).show()
             }
         })
     }
